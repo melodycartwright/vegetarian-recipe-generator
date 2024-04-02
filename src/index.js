eb7fb8873e20dc426eea61094f932f5840e6e375
melodycartwright <melody94c@gmail.com>
2024-04-02 14:24:01 +0200

    Integrated AI to generate vegetarian Recipes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-function generateRecipe(event){
-    event.preventDefault();
-   
-    let recipeResults= document.querySelector("#recipe");
+function displayRecipe(response){
+console.log("recipe generated");
     new Typewriter("#recipe", {
-      strings: "Recipe is here",
+      strings: response.data.answer,
       autoStart: true,
       delay: 1,
+      cursor:"",
     });
 }
+
+function generateRecipe(event){
+    event.preventDefault();
+
+   let instructionsInput= document.querySelector("#user-instructions");
+   let apiKey = "0229fcbo68d379b5181571fb4eff1atd";
+     let context="You are a vegetarian recipe cooking expert. You love to write delicious and easy vegetarian recipes that only use vegetarian or vegan ingredients. Your mission is to take the user instructions and create a recipe based off of it. Please display it in HTML format. Make sure to follow the user instructions"; 
+     let prompt=`User instructions: generate a vegetarian recipe based on ${instructionsInput.value}. <strong>SheCodes AI </strong> at the bottom`;
+
+   let apiUrl =
+     `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+console.log("Generating recipe");
+     console.log(`Prompt: ${prompt}`);
+console.log(`Context: ${context}`);
+
+axios.get(apiUrl).then(displayRecipe);}
+   
+    
+
 
 let recipeGeneratorForm=document.querySelector("#recipe-generator");
 
